ff98765729069fc9deb5fff54c74bcc04208e749
Alan Bedoya <alanjbedoya@gmail.com>
2020-10-24 17:52:03 -0700

    Connected my server to the client side.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Listings } from './sections';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <Listings title="Wanderhomes Listings"/>,
   document.getElementById('root')
 );
 
